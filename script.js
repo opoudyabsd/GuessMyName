2f340acce908bfdef27c3702a84d6c571d95d1fd
opoudyabsd <opoudyabsd@gmail.com>
2024-02-22 12:10:04 +0200

    Reduce

--- script.js
+++ script.js
@@ -51,7 +51,7 @@ document.querySelector('.again').addEventListener('click', function(){
     secretNumber = Math.trunc(Math.random()*20) + 1;
 
 
-    document.querySelector('.message').textContent = 'Start guessing...'
+    displayMessage ('Start guessing...') 
     document.querySelector('.score').textContent = score;
     document.querySelector('.number').textContent = "?"
     document.querySelector('.guess').value = "";
